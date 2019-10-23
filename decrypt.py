41bb1f56081cb4bb18c132d2c56843f420fd4f30
onesmithtl <56890305+onesmithtl@users.noreply.github.com>
2019-10-23 13:54:49 -0400

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -29,7 +29,9 @@ ciphertext9 = '271946f9bbb2aeadec111841a81abc300ecaa01bd8069d5cc91005e9fe4aad6e0
 ciphertext10 = '466d06ece998b7a2fb1d464fed2ced7641ddaa3cc31c9941cf110abbf409ed39598005b3399ccfafb61d0315fca0a314be138a9f32503bedac8067f03adbf3575c3b8edc9ba7f537530541ab0f9f3cd04ff50d66f1d559ba520e89a2cb2a83'
 ciphertext11 = '32510ba9babebbbefd001547a810e67149caee11d945cd7fc81a05e9f85aac650e9052ba6a8cd8257bf14d13e6f0a803b54fde9e77472dbff89d71b57bddef121336cb85ccb8f3315f4b52e301d16e9f52f904'
 
-print ('Text 1:  ', bytearray.fromhex(ciphertext1).decode())
+#print ('Text 1:  ', bytearray.fromhex(ciphertext1).decode())
+
+print ('Text 1:  ', ciphertext1)
 
 
 # print('Cipher Text 1:', message)
